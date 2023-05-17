d5fd4a12675a3c1e6f90be412dc1a7daf3d35922
Tawfik <ahmed.tawfik.ext@siemens.com>
2023-05-17 18:38:33 +0300

    :sparkles: Added PWA

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,29 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA()],
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: "Group Order",
+        short_name: "Group Order",
+        theme_color: "#121212",
+        icons: [
+          {
+            src: "/pizza.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+        background_color: "#121212",
+        description:
+          "A simple web application that allows users to create orders for making group order easier.",
+        display: "standalone",
+        lang: "en",
+        orientation: "portrait",
+        start_url: "/",
+      },
+    }),
+  ],
 });
